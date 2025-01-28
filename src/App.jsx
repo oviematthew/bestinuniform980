aad0480a1c7483876619e16b43f4524a74138145
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-01-28 09:54:11 -0500

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Navbar from "./components/navbar";
 import Dropdown from "./components/dropdown";
 
+// Check if voting is live from database
+// useEffect(() => {
+//   function checkVoting() {}
+// }, []);
+
 function App() {
+  const [votingLive, setVotingLive] = useState(true);
+
   return (
-    <>
-      <h1 className="header">Best In Uniform</h1>
-      <Dropdown labelText={"Voter's Name?"} />
-      <Dropdown labelText={"Who are you Voting For?"} />
-    </>
+    <div className="rootDiv">
+      <Navbar />
+      {votingLive && (
+        <div>
+          <h1 class="header-text">Best In Uniform</h1>
+          <Dropdown labelText={"Voter's Name?"} />
+          <Dropdown labelText={"Who are you Voting For?"} />
+        </div>
+      )}
+    </div>
   );
 }
 
