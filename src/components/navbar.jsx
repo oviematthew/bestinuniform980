532aff7cf2fb786998a771e19a9c30edcb0211b5
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-01-29 11:21:29 -0500

    added firebase config

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -77,7 +77,7 @@ export default function Navbar() {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <img
               className="h-12 w-auto"
-              src="../src/assets/logo.JPG"
+              src="../src/assets/logo.png"
               alt="Logo"
             />
           </div>
