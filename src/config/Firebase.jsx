2fd1036d367590b774b7a0f2c620645600f421e3
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-02-19 18:47:37 -0500

    updated results, voting and reset

--- src/config/Firebase.jsx
+++ src/config/Firebase.jsx
@@ -18,9 +18,9 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_storageBucket,
-  appId: import.meta.env.VITE_storageBucket,
-  measurementId: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
